b5d47a502c186e68b589b059aec2789cf5aeccab
andrew.aka.manik <andrew.aka.manik@25df3522-7982-9f5b-351b-2c3ba8ef1d51>
2015-03-09 15:27:27 +0000

    [+] Создал скрипт для генерации тестовых файлов для фазового анализа

---
+++ phase_demod_test.m
@@ -0,0 +1,16 @@
+% Частота дискретизации и длительность (в отсчетах) генерируемых файлов
+fs = 16000;
+len = fs;
+
+% Генерация стационарного полигармонического сигнала
+F0 = 100:100:5000;
+A  = 1./(1:numel(F0));
+Phi= (1:numel(F0))*2*pi/numel(F0);
+y = sum(polyharm(fs, len, F0, A, Phi),2);
+wavwrite(y*0.95/max(abs(y)), fs, 'phase_test_stat.wav');
+
+% Генерация полигармонического сигнала, с линейно увеличивающейся частотой
+% гармоник от 100 до 120 Гц
+F0 = linspace(100,120,len)'*(1:size(A,2));
+y = sum(polyharm(fs, len, F0, A, Phi),2);
+wavwrite(y*0.95/max(abs(y)), fs, 'phase_test_flow.wav');
